f49d2d49762f943d394b0663fd3a5641ba93bef3
CosmoWorker <social.tarang@gmail.com>
2025-10-25 23:58:23 +0530

    added dependencis with general ruleset defined

--- server/main.py
+++ server/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
 app=FastAPI()
+github_token=os.getenv("GITHUB_PAT_TOKEN")
+client=Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 @app.get("/")
 def main():
     print("Hello from Code reviewer server!")
 
+@app.post("/webhook") # endpoint for gh webhhook
+def handle_pr_event(payload: dict):
+    pass
 
 if __name__ == "__main__":
     main()
