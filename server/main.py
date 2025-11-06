4cf1d5bb70d820594d6ac110a8b26add241fd5ae
CosmoWorker <social.tarang@gmail.com>
2025-11-06 23:58:27 +0530

    minor formatting

--- server/main.py
+++ server/main.py
@@ -1,24 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os, logging, requests
+import os
+import logging
+import requests
 from dotenv import load_dotenv
 from groq import Groq
 
 load_dotenv()
-app=FastAPI()
+app = FastAPI()
 app.add_middleware(CORSMiddleware)
 
-logger=logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
-github_token=os.getenv("GITHUB_PAT_TOKEN")
-client=Groq(api_key=os.getenv("GROQ_API_KEY"))
+github_token = os.getenv("GITHUB_PAT_TOKEN")
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 @app.get("/")
 def main():
-    return {"msg":"Hello from code reviewer server"}
+    return {"msg": "Hello from code reviewer server"}
 
-'''
+
+"""
 Abstract Response object 
 {
   "action": "",
@@ -27,18 +30,19 @@ Abstract Response object
   "repository": {},
   "sender": {}
 }
-'''
-@app.post("/webhook") # endpoint for gh webhhook
+"""
+
+@app.post("/webhook")  # endpoint for gh webhhook
 def handle_pr_event(payload: dict):
-    pr_response=payload["pull_request"]
-    if (pr_response["state"] == "open"):
+    pr_response = payload["pull_request"]
+    if pr_response["state"] == "open":
         try:
             logger.info("Reading Rules File")
             with open("rules.txt", "r") as f:
-                ruleset=f.read()
+                ruleset = f.read()
         except Exception as e:
             logger.error("Error reading  rules file: ", e)
-        system_prompt=f"""
+        system_prompt = f"""
             You are an AI Code Reviewer. You should Review the git diff provided based on the ruleset & other facets like issues description (if provided), etc.
             Rules: 
             {ruleset}
@@ -48,32 +52,28 @@ def handle_pr_event(payload: dict):
             ### <Title (with Issue if any)>
             #### <filename>
         """
-        headers={"Authorization": f"Bearer {github_token}", "X-GitHub-Api-Version": "2022-11-28"}
-        diffs=requests.get( pr_response["diff_url"], headers=headers).text
-        user_prompt=f"""
+        headers = {
+            "Authorization": f"Bearer {github_token}",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+        diffs = requests.get(pr_response["diff_url"], headers=headers).text
+        user_prompt = f"""
             Diff:
             {diffs}
         """
-        chat_completion=client.chat.completions.create(
+        chat_completion = client.chat.completions.create(
             messages=[
-                {
-                    "role":"system",
-                    "content":system_prompt
-                },
-                {
-                    "role":"user",
-                    "content":user_prompt
-                }
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
             ],
-            model="openai/gpt-oss-120b"
+            model="openai/gpt-oss-120b",
         )
-        response=chat_completion.choices[0].message.content
-        data={
-            "body": f'''{response}'''
-        }
-        requests.post(pr_response["comments_url"], json=data,headers=headers)
+        response = chat_completion.choices[0].message.content
+        data = {"body": f"""{response}"""}
+        requests.post(pr_response["comments_url"], json=data, headers=headers)
 
-    return {"msg":"Check Done"}
+    return {"msg": "Check Done"}
+
 
 if __name__ == "__main__":
     main()
